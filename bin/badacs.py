08764a01aeb3498d9fb91344c69647b80f73385a
Brett Adams <brett.whynot@gmail.com>
2022-01-01 08:53:16 +1000

    admin save

--- bin/badacs.py
+++ bin/badacs.py
@@ -37,6 +37,7 @@ class req(PersistentServerConnectionApplication):
 
     def __init__(self, command_line, command_arg):
         PersistentServerConnectionApplication.__init__(self)
+        self.counta = counta
 
     def fixval(self,value):
         if type(value) is str:
