33da6d9502b71494edf3e00fa7ce45e9253b162d
Brett Adams <brett.whynot@gmail.com>
2022-01-07 17:56:01 +1000

    admin save

--- bin/badacs.py
+++ bin/badacs.py
@@ -244,7 +244,7 @@ class req(PersistentServerConnectionApplication):
             
             FEATURES = ['search-api','hec','s2s','search-ui','idm-ui','idm-api']
             tasks = [
-                ...({'method': 'GET', 'url': f"https://admin.splunk.com/{server}/adminconfig/v2/access/{feature}/ipallowlists", 'headers':[('Authorization',f"Bearer {token}")]} for feature in FEATURES),
+                *({'method': 'GET', 'url': f"https://admin.splunk.com/{server}/adminconfig/v2/access/{feature}/ipallowlists", 'headers':[('Authorization',f"Bearer {token}")]} for feature in FEATURES),
                 {'method': 'GET', 'url': f"https://admin.splunk.com/{server}/adminconfig/v2/access/outbound-ports", 'headers':[('Authorization',f"Bearer {token}")]}
             ]
             data = self.loop.run_until_complete(self.getall(tasks))
