d2ece7da49c40e6cbbafb1fe3626065d1a6f887c
Brett Adams <brett.whynot@gmail.com>
2022-01-07 17:18:03 +1000

    admin save

--- bin/badacs.py
+++ bin/badacs.py
@@ -35,7 +35,7 @@ class req(PersistentServerConnectionApplication):
         for req in reqs:
             task.append(asyncio.create_task(await req.json()))
         return self.loop.run_until_complete(await asyncio.gather(*tasks))
-        logger.warn(f"ACS request for {server}/adminconfig/v2/access/{feature}/ipallowlists returned {e}")
+        #logger.warn(f"ACS request for {server}/adminconfig/v2/access/{feature}/ipallowlists returned {e}")
         
 
     def fixval(self,value):
