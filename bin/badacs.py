5313b7433e8c2d40d91fac09023cb146ff7bb199
Brett Adams <brett.whynot@gmail.com>
2022-01-07 17:47:43 +1000

    admin save

--- bin/badacs.py
+++ bin/badacs.py
@@ -246,8 +246,8 @@ class req(PersistentServerConnectionApplication):
             output = {}
             
             tasks = [
-                {'method': 'GET', 'url': f"https://admin.splunk.com/{server}/adminconfig/v2/access/{feature}/ipallowlists",'headers':[('Authorization',f"Bearer {token}")]} for feature in ['search-api','hec','s2s','search-ui','idm-ui','idm-api'],
-                {'method': 'GET', 'url': f"https://admin.splunk.com/{server}/adminconfig/v2/access/outbound-ports",'headers':[('Authorization',f"Bearer {token}")]},
+                {'method': 'GET', 'url': f"https://admin.splunk.com/{server}/adminconfig/v2/access/{feature}/ipallowlists", 'headers':[('Authorization',f"Bearer {token}")]} for feature in ['search-api','hec','s2s','search-ui','idm-ui','idm-api'],
+                {'method': 'GET', 'url': f"https://admin.splunk.com/{server}/adminconfig/v2/access/outbound-ports", 'headers':[('Authorization',f"Bearer {token}")]},
             }]
             data = self.loop.run_until_complete(self.getall(tasks))
             output = data
