9f62dbe0e561d343c9fa7282d18e2a1d110acaae
Brett Adams <brett.whynot@gmail.com>
2022-06-07 15:57:47 +1000

    admin save

--- appserver/static/badacs.js
+++ appserver/static/badacs.js
@@ -321,9 +321,7 @@ const vue = new Vue({
         },
         HecAdd(token,stack){
             const payload = {
-                'spec':{
-                    'name': `Create by BADACS ${Math.floor(Math.random() * 1000)}`
-                },
+                'name': `Create by BADACS ${Math.floor(Math.random() * 1000)}`,
                 'token': token
             }
             return this.Request('change',{'stack':stack, 'method':'POST', 'endpoint':'inputs/http-event-collectors', 'data':JSON.stringify(payload)}).then((resp)=>{
